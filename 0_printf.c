fde18fc262ac1f16a8dcd8c725b47de44c0772e4
Abdo8Osama <abdo.osama301@yahoo.com>
2022-10-16 17:02:03 +0200

    chore : create main.h and  0_printf.c

---
+++ 0_printf.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * _printf - a function that produces output 
+ * according to a format..
+ * @format: character string. 
+ * Return: the number of characters printed 
+ * excluding the null byte used to end output to strings
+ */
+
+int _printf(const char *format)
+{
+	// write code here 
+	
+	
+	
+}
