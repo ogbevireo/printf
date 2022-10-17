87e2edf32b615c0546addd999bfba9551e172c7b
ogbevireo <ogbevire@gmail.com>
2022-10-17 08:18:13 +0100

    refactored the _printf format

--- _printf.c
+++ _printf.c
@@ -1,102 +1,51 @@
-#include <unistd.h>
-#include <stdio.h>
-#include <stdarg.h>
-#include "main.h"
+#include "holberton.h"
 
 /**
- * _printf - a function that produces output 
- * according to a format..
- * @format: character string. 
- * Return: the number of characters printed 
- * excluding the null byte used to end output to strings
+ * _printf - formatted output conversion and print data.
+ * @format: input string.
+ *
+ * Return: number of chars printed.
  */
+int _printf(const char *format, ...)
+{
+	unsigned int i = 0, len = 0, ibuf = 0;
+	va_list arguments;
+	int (*function)(va_list, char *, unsigned int);
+	char *buffer;
 
- 
- 
- 
-int _printf(const char *format ,...)
-{ 
-    const char *traverse; 
-    unsigned int i; 
-    char *s; 
-	double db ;
-	
-	char outbuf[2048];
-	
-    va_list arg; 
-    va_start(arg, format); 
-
-    for(traverse = format; *traverse != '\0'; traverse++) 
-    { 
-        while( *traverse != '%' ) 
-        { 
-			if( *traverse == '\0') 
-				return 1;
-            putchar(*traverse);
-            traverse++; 
-        } 
-
-        traverse++; 
-
-        switch(*traverse) 
-        { 
-			case 'f':
-                    db = va_arg(arg, double);
-                    sprintf(outbuf, "%f", db);
-                    for(int j=0;outbuf[j]!='\0';j++){
-                        putchar(outbuf[j]);
-                    }
-  
-                break;           
-		   case 'c' : i = va_arg(arg,int);     //Fetch char argument
-                        putchar(i);
-                        break; 
-			case 'i' :
-            case 'd' : i = va_arg(arg,int);   //Fetch Decimal/Integer argument
-                        if(i<0) 
-                        { 
-                            i = -i;
-                            putchar('-'); 
-                        } 
-                        puts(convert(i,10));
-                        break; 
-
-			case 'b': i = va_arg(arg,unsigned int); //Fetch binary representation
-                        puts(convert(i,2));
-                        break; 
-						
-            case 'o': i = va_arg(arg,unsigned int); //Fetch Octal representation
-                        puts(convert(i,8));
-                        break; 
-
-            case 's': s = va_arg(arg,char *);       //Fetch string
-                        puts(s); 
-                        break; 
-
-            case 'x': i = va_arg(arg,unsigned int); //Fetch Hexadecimal representation
-                        puts(convert(i,16));
-                        break; 
-        }   
-    } 
-
-    va_end(arg); 
-} 
-
-
-char *convert(unsigned int num, int base) 
-{ 
-    static char Representation[]= "0123456789ABCDEF";
-    static char buffer[50]; 
-    char *ptr; 
-
-    ptr = &buffer[49]; 
-    *ptr = '\0'; 
-
-    do 
-    { 
-        *--ptr = Representation[num%base]; 
-        num /= base; 
-    }while(num != 0); 
-
-    return(ptr); 
+	va_start(arguments, format), buffer = malloc(sizeof(char) * 1024);
+	if (!format || !buffer || (format[i] == '%' && !format[i + 1]))
+		return (-1);
+	if (!format[i])
+		return (0);
+	for (i = 0; format && format[i]; i++)
+	{
+		if (format[i] == '%')
+		{
+			if (format[i + 1] == '\0')
+			{	print_buf(buffer, ibuf), free(buffer), va_end(arguments);
+				return (-1);
+			}
+			else
+			{	function = get_print_func(format, i + 1);
+				if (function == NULL)
+				{
+					if (format[i + 1] == ' ' && !format[i + 2])
+						return (-1);
+					handl_buf(buffer, format[i], ibuf), len++, i--;
+				}
+				else
+				{
+					len += function(arguments, buffer, ibuf);
+					i += ev_print_func(format, i + 1);
+				}
+			} i++;
+		}
+		else
+			handl_buf(buffer, format[i], ibuf), len++;
+		for (ibuf = len; ibuf > 1024; ibuf -= 1024)
+			;
+	}
+	print_buf(buffer, ibuf), free(buffer), va_end(arguments);
+	return (len);
 }
