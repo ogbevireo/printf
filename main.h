2cd47aa397d6daf6ff915cf4a5def38ba67d5f68
ogbevireo <ogbevire@gmail.com>
2022-10-17 09:41:49 +0100

    main.h updated the header file

--- main.h
+++ main.h
@@ -1,2 +1,9 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+#include <stdarg.h>
+#include <stdlib.h>
+#include <unistd.h>
+
 int _printf(const char *format ,...);
 char* convert(unsigned int, int);       
