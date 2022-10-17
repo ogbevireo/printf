00d28de1acdb5ea8a46cf32a3c028925459a8c7e
ogbevireo <ogbevire@gmail.com>
2022-10-17 10:52:49 +0100

    main.h - added more libraries

--- main.h
+++ main.h
@@ -1,11 +1,37 @@
-#ifndef _MAIN_H_
-#define _MAIN_H_
+#ifndef MAIN_H
+#define MAIN_H
 
-#include <stdarg.h>
+#include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
+#include <stdarg.h>
 #include <unistd.h>
 
-int _printf(const char *format ,...);
-char* convert(unsigned int, int);
+typedef struct format_specifier FS;
 
-#endif
+/**
+ * struct format_specifier - A structure data type of format_specifier
+ * @type: Type of format specifier
+ * @parser: A function to a pointer that takes a va_list and
+ * returns a string
+ */
+
+struct format_specifier
+{
+	char type;
+	char *(*parser)(va_list);
+};
+
+unsigned int _strlen(char *s);
+char *_realloc(char *ptr, int inc);
+char *c_parser(va_list args);
+char *s_parser(va_list args);
+bool fs_register(char sp);
+void fs_buf_switcher(char sp, int j, FS *fs_buf);
+FS *gen_fs_buf(char *s, int n);
+bool check_format_specifier(char *s, bool *is_percent_fs, int *i);
+int num_of_vars(char *s);
+char *str_parser(char *format, va_list args, int *j);
+int _printf(char *format, ...);
+
+#endif /* MAIN_H */
