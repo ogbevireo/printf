dddcb3008f9d1ed194a2378fc4d05e4131b9101f
Abdo8Osama <abdo.osama301@yahoo.com>
2022-10-18 17:58:32 +0200

    printf

--- get_print.c
+++ get_print.c
@@ -10,7 +10,7 @@
  * printing function
  * Return: a pointer to the matching printing function
  */
-int (*get_print(char s))(va_list l, flags_t *f, int width, int precision, int length)
+int (*get_print(char s))(va_list , flags_t *, int , int , int )
 {
 	ph func_arr[] = {
 		{'i', print_int},
